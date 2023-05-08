ff11c9b916e152562ee2daa9000b7f946e394952
Cireael <oren.si@yandex.ru>
2023-05-08 22:25:01 +0300

    first

---
+++ TikTok/VideoTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  VideoTableViewCell.swift
+//  TikTok
+//
+//  Created by Julia on 08.04.2023.
+//
+
+import BMPlayer
+import SnapKit
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+    
+    private var link: String
+    private var player = BMPlayer()
+    
+    init(link: String) {
+        self.link = link
+        super.init(style: .default, reuseIdentifier: "cell")
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initialize() {
+        player.isUserInteractionEnabled = false
+        BMPlayerConf.enableVolumeGestures = false
+        BMPlayerConf.enableBrightnessGestures = false
+        BMPlayerConf.enablePlaytimeGestures = false
+        BMPlayerConf.shouldAutoPlay = true
+        
+        let resource = BMPlayerResource(url: URL(string: link)!)
+        player.setVideo(resource: resource)
+        self.contentView.addSubview(player)
+        player.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+    }
+    
+}
